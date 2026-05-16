6854b3182ec77b04521ef1d36c41565762343d32
Michał Brzeziński <michal.brzezinski.off@gmail.com>
2026-05-16 11:15:45 +0000

    clientutils module refactor

--- internal/clientutils/ack_timer.go
+++ internal/clientutils/ack_timer.go
@@ -4,15 +4,20 @@ import (
 	"time"
 )
 
-// StartAckTimer starts a 5-second timer for a given message ID.
-// If the "done" channel is not closed within 5 seconds, onTimeout is called.
+// DefaultAckTimeout defines how long the client waits for MEOW_OK
+// before considering the message undelivered.
+const DefaultAckTimeout = 5 * time.Second
+
+// StartAckTimer starts a timeout watcher for a given message ID.
+// If the "done" channel is closed before the timeout, the timer exits silently.
+// If the timeout expires first, onTimeout is invoked.
 func StartAckTimer(msgID int64, done <-chan struct{}, onTimeout func()) {
 	go func() {
 		select {
 		case <-done:
 			// ACK received, timer cancelled.
 			return
-		case <-time.After(5 * time.Second):
+		case <-time.After(DefaultAckTimeout):
 			onTimeout()
 		}
 	}()
